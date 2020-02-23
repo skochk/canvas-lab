b6f0263ca3fc89541bae7821eda947e965a3a0c5
skochk <kochkares@gmail.com>
2020-02-24 01:01:55 +0200

    testing

--- main.js
+++ main.js
@@ -10,103 +10,133 @@ document.addEventListener('DOMContentLoaded',function(){
         y: 150,
     }
 
-    print(smallRadius,bigRadius);
+    
+    let a = calcArray(smallRadius,bigRadius);
 
-    let arr = [];
+    print(a);
+
     document.querySelector('.smallButton').addEventListener('click',function(){
         smallRadius = document.querySelector('.small').value;
         context.clearRect(0, 0, canvas.width, canvas.height);
-        print(smallRadius,bigRadius);
+        let a = calcArray(smallRadius,bigRadius);
+        print(a);
     });
 
     document.querySelector('.bigButton').addEventListener('click', function(){
         bigRadius = document.querySelector('.big').value;
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        print(smallRadius,bigRadius);
+        context.clearRect-(0, 0, canvas.width, canvas.height);
+        let a = calcArray(smallRadius,bigRadius);
+        print(a);
+
     })
 
 
 
-    function print(smallRadius,bigRadius){
-        
+    function print(a){
+        context.beginPath();
         context.lineWidth = 5;
-        context.strokeStyle = "black";
-              
-
-    
-        // main figure 
-            context.moveTo(figurePos.x-50, figurePos.y-25);
-            context.lineTo(figurePos.x,figurePos.y-50);
-            context.lineTo(figurePos.x+50,figurePos.y-25);
-            context.lineTo(figurePos.x+50,figurePos.y+25);
-            context.lineTo(figurePos.x,figurePos.y+50);
-            context.lineTo(figurePos.x-50,figurePos.y+25);
-            context.lineTo(figurePos.x-50,figurePos.y-27);
+        for(let i = 0; i < a.length; i++){
+            if(a[i][2] == "move"){
+                context.stroke();
+                context.moveTo(a[i][0],a[i][1]);
+            }else{
+                context.lineTo(a[i][0],a[i][1]);
+            }
             context.stroke();
+        }
+        context.stroke();
+        context.closePath();
+        
+    }
+
+    function calcArray(smallRadius,bigRadius){
+        let arr = [];
+        // context.lineWidth = 5;
+        // context.strokeStyle = "black";
+      
+        // arr.push("move");
+        arr.push([figurePos.x-50, figurePos.y-25,"move"])
+        arr.push([figurePos.x,figurePos.y-50]);
+        arr.push([figurePos.x+50,figurePos.y-25]);
+        arr.push([figurePos.x+50,figurePos.y+25]);
+        arr.push([figurePos.x,figurePos.y+50]);
+        arr.push([figurePos.x-50,figurePos.y+25]);
+        arr.push([figurePos.x-50,figurePos.y-27]);
+        // context.stroke();
 
 
         var step = 2 * Math.PI / 20; // примечание 2 
         let Xpos, Ypos;
-        context.font = '9px Arial';
+        // context.font = '9px Arial';
 
 
         //center circle small
-        context.beginPath();
+        // context.beginPath();
+        arr.push([Math.cos(0) * smallRadius+figurePos.x, Math.sin(0) * smallRadius+figurePos.y ,"move" ]);
         for(var angle = 0; angle <= 2 * Math.PI; angle += step) {
             Xpos = Math.cos(angle) * smallRadius;
-            Ypos = Math.sin(angle) * smallRadius;
-            context.lineTo(Xpos+figurePos.x,Ypos+figurePos.y);
-            // console.log(Xpos, Ypos, angle);
+            Ypos = Math.sin(angle) * smallRadius;   
+            // context.lineTo(Xpos+figurePos.x,Ypos+figurePos.y);
+            arr.push([Xpos+figurePos.x,Ypos+figurePos.y]);
         }
-        context.stroke();
+        // context.stroke();
 
 
 
         //center bottom small circle
-        context.beginPath();
+        // context.beginPath();
+        arr.push([Math.cos(- 1/7 * Math.PI) * smallRadius+figurePos.x, Math.sin(- 1/7 * Math.PI) * smallRadius+figurePos.y + 50  ,"move"]);
         for(var angle = - 1/7 * Math.PI; angle <= 10/8 * Math.PI; angle += step) {
             Xpos = Math.cos(angle) * smallRadius;
             Ypos = Math.sin(angle) * smallRadius;
-            context.lineTo(Xpos+figurePos.x,Ypos+figurePos.y+50);       
+            // context.lineTo(Xpos+figurePos.x,Ypos+figurePos.y+50);    
+            arr.push([Xpos+figurePos.x,Ypos+figurePos.y+50]);
         }
-        context.stroke();
+        // context.stroke();
         
         
         //top circle
-        context.beginPath();
+        // context.beginPath();
+       arr.push([Math.cos(7/8 * Math.PI)  * smallRadius + figurePos.x,   Math.sin(7/8 * Math.PI) * smallRadius+figurePos.y-50  ,"move"]);
         for(var angle = 7/8 * Math.PI; angle <= 22/10 * Math.PI; angle += step) {
             Xpos = Math.cos(angle) * smallRadius;
             Ypos = Math.sin(angle) * smallRadius;
-            context.lineTo(Xpos+figurePos.x,Ypos+figurePos.y-50);
+            // context.lineTo(Xpos+figurePos.x,Ypos+figurePos.y-50);
+            arr.push([Xpos+figurePos.x,Ypos+figurePos.y-50])
         }
-        context.stroke();
+        // context.stroke();
 
 
         //left big circle
-        context.beginPath();
+        // context.beginPath();
+        arr.push([Math.cos(1/13 * Math.PI)  * smallRadius + figurePos.x - 50,   Math.sin(1/13 * Math.PI) * smallRadius+figurePos.y+30 ,"move"]);
         for(var angle = 1/13 * Math.PI; angle <= 3/2 * Math.PI; angle += step) {
             Xpos = Math.cos(angle) * bigRadius;
             Ypos = Math.sin(angle) * bigRadius;
-            context.lineTo(Xpos+figurePos.x-50,Ypos+figurePos.y+30);
+            // context.lineTo(Xpos+figurePos.x-50,Ypos+figurePos.y+30);
+            arr.push([Xpos+figurePos.x-50,Ypos+figurePos.y+30]);
            
         }
-        context.stroke();
+        // context.stroke();
 
             
         //right big circle
-        context.beginPath();
+        // context.beginPath();
+        arr.push([Math.cos(-1/2 * Math.PI)  * smallRadius + figurePos.x + 50,   Math.sin(-1/2 * Math.PI) * smallRadius+figurePos.y+30 ,"move"]);
         for(var angle = -1/2 * Math.PI; angle <= 9/10 * Math.PI; angle += step) {
             Xpos = Math.cos(angle) * bigRadius;
             Ypos = Math.sin(angle) * bigRadius;
-            context.lineTo(Xpos+figurePos.x+50,Ypos+figurePos.y+30);
+            // context.lineTo(Xpos+figurePos.x+50,Ypos+figurePos.y+30);
+            arr.push([Xpos+figurePos.x+50,Ypos+figurePos.y+30]);
         } 
-        context.stroke();
-        context.beginPath();
+        // context.stroke();
+        // context.beginPath();
 
+        return arr;
 
     }
 
-//     // vertical grid loop
+    // vertical grid loop
 // for(let i = 0.5; i<canvas.offsetWidth; i=i+20){
 //     context.lineWidth = 1;
 //     context.strokeStyle = "black";
